edc8bb13914227f43854c88f91dd693025119d00
KM9ACE <dimchea50@gmail.com>
2025-04-04 21:43:03 +0900

    수퍼두퍼울트라 윈도우창 2개 시제2

--- Project1/20221416 console.c
+++ Project1/20221416 console.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <windows.h>
 
-//아스키 코드로 색상 설정
-void setTextColor(int color) 
-{
-    printf("\033[38;5;%dm", color); // 텍스트 색상 변경
-}
-
+// 아스키 코드로 배경 색상 설정
 void setBackgroundColor(int color) 
 {
-    printf("\033[48;5;%dm", color); // 배경 색상 변경
+    printf("\033[48;5;%dm", color);
 }
 
-void resetColor() 
+// 색상 초기화
+resetColor() 
 {
-    printf("\033[0m"); // 색상 초기화
+    printf("\033[0m");
 }
 
+//커서 위치 조정
 void gotoxy(int x, int y)
 {
     COORD Cur;
@@ -26,28 +23,32 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(output, Cur);
 }
 
-// 바탕화면을 배경 색상으로 채우는 함수
+// 바탕화면 채우는 함수
 void fillBackground(int color) 
 {
-    system("cls");  // 콘솔 창을 클리어
-    setBackgroundColor(color);  // 배경 색상 설정
-    for (int i = 0; i < 40; i++) 
-    {  // 대략 40줄 출력
-        for (int j = 0; j < 100; j++) 
-        {  // 대략 100칸씩 출력
+    system("cls");
+    setBackgroundColor(color);
+
+    int i = 0;
+	int j = 0;
+
+    for (i = 0; i < 15; i++) 
+    {
+        for (j = 0; j < 120; j++) 
+        {
             printf(" ");
         }
-        printf("\n");
     }
 }
 
-// 윈도우 구조체 정의
+// 윈도우 구조
 struct Window 
 {
     int width;
     int height;
     int x;
     int y;
+    int name;
     int backgroundColor;  // 배경 색상 (테두리 색상과 동일)
 };
 
@@ -57,16 +58,17 @@ void drawWindow(struct Window w)
     // 윈도우 배경 색상 설정
     setBackgroundColor(w.backgroundColor);
 
+	int i = 0;
+	int j = 0;
+
     // 윈도우 테두리와 내부를 그리기
-    for (int i = 0; i < w.width; i++) 
+    for (i = 0; i < w.width; i++) 
     {
         gotoxy(w.x + i, w.y);  // 상단
         printf("-");
-
-        gotoxy(w.x + i, w.y + w.height - 1);  // 하단
-        printf("-");
     }
-    for (int i = 0; i < w.height; i++) 
+
+    for (i = 0; i < w.height; i++) 
     {
         gotoxy(w.x, w.y + i);  // 왼쪽
         printf("|");
@@ -75,28 +77,33 @@ void drawWindow(struct Window w)
         printf("|");
     }
 
+    for (i = 0; i < w.width; i++)
+	{
+        gotoxy(w.x + i, w.y + w.height - 1);  // 하단
+        printf("-");
+    }
+
     // 윈도우 내부를 배경 색상으로 채우기
-    for (int i = 1; i < w.height - 1; i++) 
+    for (i = 1; i < w.height - 1; i++) 
     {
-        for (int j = 1; j < w.width - 1; j++) 
+        for (j = 1; j < w.width - 1; j++) 
         {
             gotoxy(w.x + j, w.y + i);
             printf(" ");
         }
     }
-
+	printf("\n\n\n");
     resetColor();  // 색상 초기화
 }
 
-int main() 
-{
+int main() {
     // 배경을 녹색으로 채우기
-    fillBackground(22);  // 배경 색상은 초록색 (22)
+    fillBackground(22); // 초록색(22)
 
     // 첫 번째 윈도우 (하늘색 배경)
-    struct Window window1 = { 20, 7, 10, 3, 45 };  // 하늘색 배경 (테두리도 하늘색)
+    struct Window window1 = { 20, 7, 10, 3, 'A', 45}; // 하늘색(45)
     // 두 번째 윈도우 (노란색 배경)
-    struct Window window2 = { 20, 7, 15, 5, 226 };  // 노란색 배경 (테두리도 노란색)
+    struct Window window2 = { 20, 7, 15, 5, 'B', 226}; // 노란색(226)
 
     // 첫 번째 윈도우 그리기
     drawWindow(window1);
