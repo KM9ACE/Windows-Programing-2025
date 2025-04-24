334437c87d32f10c3e9c6f10297d7b8375f5f6b0
KM9ACE <dimchea50@gmail.com>
2025-04-24 11:23:18 +0900

    １、 ２번

--- MidTermTest/Problem1/main.c
+++ MidTermTest/Problem1/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+int A = 0;
+
+void PassOrFail() 
+{
+    int myPredictedScore = 0;
+
+    if (myPredictedScore >= 50) 
+    {
+        A = 1;
+    }
+    else 
+    {
+        A = 0;
+    }
+}
+
 int main() 
 {
+    PassOrFail();
 
+    if (A == 1) 
+    {
+        printf("재시험 없습니다!\n");
+    }
+    else 
+    {
+        printf("우리는 망했다... 재시험이다...\n");
+    }
+
+    return 0;
 }
