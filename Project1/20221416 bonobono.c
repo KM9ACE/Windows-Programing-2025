cd674523fa4b52537106f94b2cbdcaa1541364ac
KM9ACE <dimchea50@gmail.com>
2025-05-16 19:35:20 +0900

    보노보노 수염 추가 전

--- Project1/20221416 bonobono.c
+++ Project1/20221416 bonobono.c
@@ -1,71 +1,82 @@
-#pragma comment(lib, "Opengl32.lib")
-// ! F I R S T  IN  L A S T  O U T ! //
+#pragma comment(lib, "opengl32.lib")
+
 #include <GLFW/glfw3.h>
 #include <math.h>
 
 #define PI 3.1415926535f
 
-typedef struct
+typedef struct 
 {
     float x, y;
 } Vec2;
 
-typedef struct
+typedef struct 
 {
     Vec2 position;
     float rotation;
-    Vec2 scale;
+    Vec2 scale;  // 반지름(픽셀 단위)
 } Transform;
 
-typedef struct
+typedef struct 
 {
     Transform transform;
     int segments;
 } Ellipse;
 
-void draw_ellipse(Ellipse* e)
+void draw_ellipse(Ellipse* e, float r, float g, float b) 
 {
-    glPushMatrix();
-
+    glColor3f(r, g, b);
     glBegin(GL_TRIANGLE_FAN);
-    glColor3f(0.0f, 0.439f, 0.749f);  // #0070c0 색상 설정
-    glVertex2f(0.0f, 0.0f);  // 중심점
-
-    for (int i = 0; i <= e->segments; ++i)
+    glVertex2f(e->transform.position.x, e->transform.position.y);
+    for (int i = 0; i <= e->segments; ++i) 
     {
         float angle = 2.0f * PI * i / e->segments;
-        glVertex2f(cosf(angle) * e->transform.scale.x, sinf(angle) * e->transform.scale.y);
+        float x = e->transform.position.x + cosf(angle) * e->transform.scale.x;
+        float y = e->transform.position.y + sinf(angle) * e->transform.scale.y;
+        glVertex2f(x, y);
     }
-
     glEnd();
-
-    glPopMatrix();
 }
 
-int main()
+int main() 
 {
     if (!glfwInit())
         return -1;
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL C Demo", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(537, 445, "OpenGL Ellipses", NULL, NULL);
+    if (!window) 
     {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-    glOrtho(-1, 1, -1, 1, -1, 1); // 2D
 
-    Ellipse ell = { {{0.0f, 0.0f}, 0, {430.0f / 800.0f, 387.0f / 600.0f}}, 32 };
+    // 픽셀 좌표계로 설정
+    glOrtho(0, 537, 0, 445, -1, 1);
+    glClearColor(1, 1, 1, 1);  // 배경 흰색
 
-    while (!glfwWindowShouldClose(window))
+    Ellipse borderEllipse = { { {537 / 2.0f, 445 / 2.0f}, 0, {215, 193.5f} }, 64 };
+    Ellipse fillEllipse = { { {537 / 2.0f, 445 / 2.0f}, 0, {213, 191} }, 64 };
+    Ellipse eyeEllipse1 = { { {89, 268}, 0, {15, 15.5f} }, 48 };
+    Ellipse eyeEllipse2 = { { {428, 262}, 0, {16, 15} }, 48 };
+    Ellipse whiteEllipse1 = { { {236, 157}, 0, {41.5f, 37.5f} }, 64 };
+    Ellipse whiteEllipse2 = { { {314, 157}, 0, {41.5f, 37.5f} }, 64 };
+    Ellipse whiteEllipse3 = { { {275, 191}, 0, {25, 20} }, 64 };
+    Ellipse noseEllipse = { { {275, 201}, 0, {33, 27.5f} }, 64 };
+
+    while (!glfwWindowShouldClose(window)) 
     {
-        float time = (float)glfwGetTime();
-
         glClear(GL_COLOR_BUFFER_BIT);
 
-        draw_ellipse(&ell);  // 타원 그리기
+        draw_ellipse(&borderEllipse, 0, 0, 0);
+        draw_ellipse(&fillEllipse, 0, 0.439f, 0.753f);
+        draw_ellipse(&eyeEllipse1, 0, 0, 0);
+        draw_ellipse(&eyeEllipse2, 0, 0, 0);
+        draw_ellipse(&whiteEllipse1, 1, 1, 1);
+        draw_ellipse(&whiteEllipse2, 1, 1, 1);
+        draw_ellipse(&whiteEllipse3, 1, 1, 1);  // 뒤에 있는 흰 타원
+        draw_ellipse(&noseEllipse, 0, 0, 0);    // 앞에 있는 검은 타원
 
         glfwSwapBuffers(window);
         glfwPollEvents();
